3370ecf52746f693af429e6fceb925d6f4ea1f84
Gabriel Monroy <gabriel@opdemand.com>
2013-07-26 12:30:06 -0600

    remove admin urls #37

--- deis/urls.py
+++ deis/urls.py
@@ -18,8 +18,6 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('api.urls')),
     url(r'^', include('web.urls')),
 )
