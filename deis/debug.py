578c032d48b7260ddc9272e10a5620c32b3c73b6
Matt Boersma <matt@opdemand.com>
2013-07-29 17:40:54 -0600

    Removed pydevd debug.py file from master branch.

--- deis/debug.py
+++
@@ -1,9 +0,0 @@
-
-from pydevd import pydevd
-
-
-pydevd.settrace('box.techverity.com', stdoutToServer=True, stderrToServer=True,
-                port=33000, suspend=False, trace_only_current_thread=False)
-
-
-from .settings import *  # noqa
