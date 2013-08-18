976ba8849331bafd6663e718d5529dfbca68b6d7
Matt Boersma <matt@opdemand.com>
2013-08-18 09:11:31 -0600

    Aligned the Deis web UI with the deis.io/docus theme.

--- deis/settings.py
+++ deis/settings.py
@@ -137,6 +137,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.humanize',
     'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.sites',
