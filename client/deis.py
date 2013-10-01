4836b998fb6bb627f00bd58093d33e7414b20fe2
Gabriel Monroy <gabriel@opdemand.com>
2013-10-01 16:17:31 -0600

    switch master to v0.1.1

--- client/deis.py
+++ client/deis.py
@@ -63,7 +63,7 @@ from docopt import DocoptExit
 import requests
 import tempfile
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 class Session(requests.Session):
--- deis/__init__.py
+++ deis/__init__.py
@@ -1,7 +1,7 @@
 """
 The Deis main package, including the top-level URLs, Django project
 settings, and WSGI setup. Most application domain-specific code lives in
-the api, celerytasks, and web Django apps.
+the api, provider, cm, and web Django apps.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
