5f6df3be0e1b01de718071a1f0c459018f2ab87a
Scott Stamp <scottstamp851@gmail.com>
2013-11-01 16:12:33 -0230

    PEP8 Formatting

--- client/deis.py
+++ client/deis.py
@@ -1191,46 +1191,57 @@ class DeisClient(object):
         """
         path = args.get('<key>')
         if not path:
-          ssh_dir = os.path.expanduser('~/.ssh')
-          pubkeys = glob.glob(os.path.join(ssh_dir, '*.pub'))
-          pubkeys_list = []
-          if not pubkeys:
-            print('No SSH public keys found')
-            return
-          print('Found the following SSH public keys:')
-          for i, k in enumerate(pubkeys):
-            key = k.split(os.path.sep)[-1]
-            with open(k) as f:
-              data = f.read()
-              match = re.match(r'^(ssh-...) ([^ ]+) ?(.*)', data)
-              if not match:
-                print("Could not parse SSH public key {0}".format(key))
+            ssh_dir = os.path.expanduser('~/.ssh')
+            pubkeys = glob.glob(os.path.join(ssh_dir, '*.pub'))
+            pubkeys_list = []
+            if not pubkeys:
+                print('No SSH public keys found')
                 return
-              key_type, key_str, _key_comment = match.groups()
-              pubkeys_list.append([k, key, key_type, key_str, _key_comment])
-              print("{0}) {1} {2}".format(i + 1, key, _key_comment))
+            print('Found the following SSH public keys:')
+            for i, k in enumerate(pubkeys):
+                key = k.split(os.path.sep)[-1]
+                with open(k) as f:
+                    data = f.read()
+                    match = re.match(r'^(ssh-...) ([^ ]+) ?(.*)', data)
+                    if not match:
+                        print("Could not parse SSH public key {0}".format(key))
+                        return
+                    key_type, key_str, _key_comment = match.groups()
+                    pubkeys_list.append([k, key, key_type, key_str, _key_comment])
+                    print("{0}) {1} {2}".format(i + 1, key, _key_comment))
 
-          inp = raw_input('Which would you like to use with Deis? ')
-          try:
-            selected_key = pubkeys_list[int(inp) - 1]
-            path = selected_key[0]
-          except:
-            print('Aborting')
-            return
+            inp = raw_input('Which would you like to use with Deis? ')
+            try:
+                selected_key = pubkeys_list[int(inp) - 1]
+                path = selected_key[0]
+            except:
+                print('Aborting')
+                return
 
-          if not selected_key[4]:
-            body = {'id': selected_key[1].replace('.pub', ''), 'public': "{0} {1}".format(selected_key[2], selected_key[3])}
-            sys.stdout.write("Uploading {} to Deis...".format(selected_key[1]))
-          else:
-            body = {'id': selected_key[4], 'public': "{0} {1}".format(selected_key[2], selected_key[3])}
-            sys.stdout.write("Uploading {} to Deis...".format(selected_key[4]))
-          sys.stdout.flush()
-          response = self._dispatch('post', '/api/keys', json.dumps(body))
-          if response.status_code == requests.codes.created:  # @UndefinedVariable
-            print('done')
-          else:
-            raise ResponseError(response)
-
+            if not selected_key[4]:
+                body = {
+                    'id': selected_key[1].replace('.pub', ''),
+                    'public': "{0} {1}".format(
+                        selected_key[2], selected_key[3]
+                    )
+                }
+                
+                sys.stdout.write("Uploading {} to Deis...".format(selected_key[1]))
+            else:
+                body = {
+                    'id': selected_key[4],
+                    'public': "{0} {1}".format(
+                        selected_key[2], selected_key[3]
+                    )
+                }
+                
+                sys.stdout.write("Uploading {} to Deis...".format(selected_key[4]))
+            sys.stdout.flush()
+            response = self._dispatch('post', '/api/keys', json.dumps(body))
+            if response.status_code == requests.codes.created:  # @UndefinedVariable
+                print('done')
+            else:
+                raise ResponseError(response)
 
     def keys_list(self, args):
         """
