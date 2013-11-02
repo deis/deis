03ed3681a70a31e8fd1b426606d59910b7e6d8af
Matt Boersma <matt@opdemand.com>
2013-11-02 09:18:48 -0600

    Updated Berksfile format for 3.0.0 beta 3 and Chef to 11.6.2.

--- Berksfile
+++ Berksfile
@@ -1,4 +1,4 @@
-site :opscode
+source "http://api.berkshelf.com"
 
 cookbook 'apt'
 cookbook 'rsyslog'
--- contrib/vagrant/provision-controller.sh
+++ contrib/vagrant/provision-controller.sh
@@ -25,7 +25,7 @@ fi
 #################
 node_name=deis-controller
 run_list="recipe[deis::controller]"
-chef_version=11.4.4
+chef_version=11.6.2
 
 ################
 # SSH settings #
