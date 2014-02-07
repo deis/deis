2a5439e40c01b4fc7327877ab053c1fc38808971
Matt Boersma <matt@opdemand.com>
2014-02-07 13:36:32 -0700

    Added static file serving WSGI app, fixes #507.

--- deis/wsgi.py
+++ deis/wsgi.py
@@ -6,29 +6,33 @@ and any production WSGI deployments. It should expose a module-level variable
 named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
 this application via the ``WSGI_APPLICATION`` setting.
 
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
 """
 
 from __future__ import unicode_literals
 import os
 
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "deis.settings"
+from django.core.wsgi import get_wsgi_application
+import static
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deis.settings")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+class Dispatcher(object):
+    """
+    Dispatches requests between two WSGI apps, a static file server and a
+    Django server.
+    """
+
+    def __init__(self):
+        self.django_handler = get_wsgi_application()
+        self.static_handler = static.Cling(os.path.dirname(os.path.dirname(__file__)))
+
+    def __call__(self, environ, start_response):
+        if environ['PATH_INFO'].startswith('/static'):
+            return self.static_handler(environ, start_response)
+        else:
+            return self.django_handler(environ, start_response)
+
+
+application = Dispatcher()
