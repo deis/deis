2df6f9ac1b3d5d415f69ac190747cbbc0d185577
Matt Boersma <matt@opdemand.com>
2014-02-12 21:23:09 -0700

    Switch master to v0.5.1.

--- client/deis.py
+++ client/deis.py
@@ -71,7 +71,7 @@ from docopt import DocoptExit
 import requests
 import yaml
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 
 
 locale.setlocale(locale.LC_ALL, '')
--- client/setup.py
+++ client/setup.py
@@ -28,7 +28,7 @@ else:
 
 
 setup(name='deis',
-      version='0.5.0',
+      version='0.5.1',
       license=APACHE_LICENSE,
       description='Command-line Client for Deis, the open PaaS',
       author='OpDemand',
