7701372aa44792ee53311a6ba5dc1d9f4a54d900
Gabriel Monroy <gabriel@opdemand.com>
2014-04-14 21:16:07 -0600

    feat(logger): add new syslog daemon based on https://github.com/ziutek/syslog

---
+++ logger/syslog/message.go
@@ -0,0 +1,43 @@
+package syslog
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+type Message struct {
+	Time   time.Time
+	Source net.Addr
+	Facility
+	Severity
+	Timestamp time.Time // optional
+	Hostname  string    // optional
+	Tag       string    // message tag as defined in RFC 3164
+	Content   string    // message content as defined in RFC 3164
+	Tag1      string    // alternate message tag (white rune as separator)
+	Content1  string    // alternate message content (white rune as separator)
+}
+
+// NetSrc only network part of Source as string (IP for UDP or Name for UDS)
+func (m *Message) NetSrc() string {
+	switch a := m.Source.(type) {
+	case *net.UDPAddr:
+		return a.IP.String()
+	case *net.UnixAddr:
+		return a.Name
+	case *net.TCPAddr:
+		return a.IP.String()
+	}
+	// Unknown type
+	return m.Source.String()
+}
+
+func (m *Message) String() string {
+	timeLayout := "2006-01-02 15:04:05"
+	return fmt.Sprintf(
+		"%s %s %s %s%s",
+		m.Time.Format(timeLayout), m.Source,
+		m.Hostname, m.Tag, m.Content,
+	)
+}
---
+++ logger/syslogd/syslogd.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"path"
+	"regexp"
+	"syscall"
+	"syslog"
+)
+
+const logRoot = "/var/log/deis"
+
+type handler struct {
+	// To simplify implementation of our handler we embed helper
+	// syslog.BaseHandler struct.
+	*syslog.BaseHandler
+}
+
+// Simple fiter for named/bind messages which can be used with BaseHandler
+func filter(m *syslog.Message) bool {
+	// return m.Tag == "named" || m.Tag == "bind"
+	return true
+}
+
+func newHandler() *handler {
+	h := handler{syslog.NewBaseHandler(5, filter, false)}
+	go h.mainLoop() // BaseHandler needs some gorutine that reads from its queue
+	return &h
+}
+
+// check if a file path exists
+func fileExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func getLogFile(m *syslog.Message) (io.Writer, error) {
+	r := regexp.MustCompile(`^.* ([a-z-]+)\[[a-z0-9\.]+\].*`)
+	match := r.FindStringSubmatch(m.String())
+	if match == nil {
+		return nil, errors.New("Could not find app name in message")
+	}
+	appName := match[1]
+	filePath := path.Join(logRoot, appName+".log")
+	// check if file exists
+	exists, err := fileExists(filePath)
+	if err != nil {
+		return nil, err
+	}
+	// return a new file or the existing file for appending
+	var file io.Writer
+	if exists {
+		file, err = os.OpenFile(filePath, os.O_RDWR|os.O_APPEND, 0644)
+	} else {
+		file, err = os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0644)
+	}
+	return file, err
+}
+
+func writeToDisk(m *syslog.Message) error {
+	file, err := getLogFile(m)
+	if err != nil {
+		return err
+	}
+	bytes := []byte(m.String() + "\n")
+	file.Write(bytes)
+	return nil
+}
+
+// mainLoop reads from BaseHandler queue using h.Get and logs messages to stdout
+func (h *handler) mainLoop() {
+	for {
+		m := h.Get()
+		if m == nil {
+			break
+		}
+		fmt.Println(m)
+		err := writeToDisk(m)
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("Exit handler")
+	h.End()
+}
+
+func main() {
+	// Create a server with one handler and run one listen gorutine
+	s := syslog.NewServer()
+	s.AddHandler(newHandler())
+	s.Listen("0.0.0.0:514")
+
+	// Wait for terminating signal
+	sc := make(chan os.Signal, 2)
+	signal.Notify(sc, syscall.SIGTERM, syscall.SIGINT)
+	<-sc
+
+	// Shutdown the server
+	fmt.Println("Shutdown the server...")
+	s.Shutdown()
+	fmt.Println("Server is down")
+}
