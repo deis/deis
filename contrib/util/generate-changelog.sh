2bce3f4eb5ae6d877bf6f3541c9b8171d12c6aeb
Matthew Fisher <matthewf@opdemand.com>
2014-05-09 20:47:13 -0700

    feat(contrib): add changelog script
    This script assists in generating the changelog from one release
    to another.
    
    To retrieve the changelog from a specific tag to HEAD:
    
        $ ./generate-changelog.sh <tag>
    
    To retrieve the changelog from two different tags:
    
        $ ./generate-changelog.sh <from> <to>

---
+++ contrib/util/generate-changelog.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+usage() {
+    echo "Usage: $0 <from> [to]"
+}
+
+retrieve() {
+    git --no-pager log --oneline --no-merges --grep="$1" $FROM..$TO
+}
+
+subheading() {
+    echo "## $1\n"
+    retrieve "$2"
+    echo
+}
+
+FROM=$1
+TO=${2:-"HEAD"}
+
+if [ -z $1 ];
+then
+    usage
+    exit 1
+fi
+
+echo "# $FROM -> $TO\n"
+
+subheading "Features" "feat("
+subheading "Fixes" "fix("
+subheading "Documentation" "docs("
