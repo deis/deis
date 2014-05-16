2dee62cc1a9f6579d55884c638ec08f13622d126
Gabriel Monroy <gabriel@opdemand.com>
2014-05-16 09:42:01 -0600

    fix(scheduler): detect exposed ports from image for PORT envvar

--- controller/scheduler/coreos.py
+++ controller/scheduler/coreos.py
@@ -197,7 +197,7 @@ Description={name}
 [Service]
 ExecStartPre=/usr/bin/docker pull {image}
 ExecStartPre=/bin/sh -c "docker inspect {name} >/dev/null 2>&1 && docker rm -f {name} || true"
-ExecStart=/usr/bin/docker run --name {name} -P {image} {command}
+ExecStart=/bin/sh -c "port=$(docker inspect -f '{{{{range $k, $v := .config.ExposedPorts }}}}{{{{$k}}}}{{{{end}}}}' {image} | cut -d/ -f1) ; /usr/bin/docker run --name {name} -P -e PORT=$port {image} {command}"
 ExecStartPost=/bin/sh -c "until docker inspect {name} >/dev/null 2>&1; do sleep 1; done"; \
     /bin/sh -c "arping -Idocker0 -c1 `docker inspect -f '{{{{ .NetworkSettings.IPAddress }}}}' {name}`"
 ExecStop=/usr/bin/docker rm -f {name}
