a3ed364aac7b43ea4ac3ecf59b588065639bf0f0
Matthew Fisher <matthewf@opdemand.com>
2014-06-06 15:31:32 -0600

    fix(controller) disable django admin URL
    If you accidentally create an object that requires an external library
    such as the Release object, no docker image will be pushed to the registry,
    which is unintentional. This disables the django admin URL such that you can't
    shoot yourself in the :shoe:.

--- controller/deis/urls.py
+++ controller/deis/urls.py
@@ -18,9 +18,12 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('api.urls')),
 )
 
 if settings.WEB_ENABLED:
-    urlpatterns += patterns('', url(r'^', include('web.urls')))
+    urlpatterns += patterns(
+        '',
+        url(r'^', include('web.urls')),
+        url(r'^admin/', include(admin.site.urls)),
+    )
