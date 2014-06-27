b8629eff29b28464d2843b58d2be1b1f968f4caa
Matthew Fisher <matthewf@opdemand.com>
2014-06-27 14:39:44 -0700

    refactor(controller): remove urlparse logic
    The logic has been moved over to the registry, so it's not needed any more.

--- controller/api/tasks.py
+++ controller/api/tasks.py
@@ -41,23 +41,8 @@ def deploy_release(app, release):
 def import_repository(source, target_repository):
     """Imports an image from a remote into our own private registry"""
 
-    url = urlparse(source)
-    scheme = url.scheme if url.scheme else 'http'
-    # strip the leading slash
-    src_repository = url.path[1:]
-
-    if url.hostname and url.port:
-        src_index = '{}://{}:{}'.format(scheme, url.hostname, url.port)
-    elif url.hostname:
-        src_index = '{}://{}'.format(scheme, url.hostname)
-    else:
-        # assume just the repository name was given, therefore it came from
-        # the public index
-        src_index = settings.PUBLIC_INDEX_URL
-        src_repository = source
     data = {
-        'src_index': src_index,
-        'src_repository': src_repository,
+        'src': source,
     }
     response = requests.post(
         '{}/v1/repositories/{}/tags'.format(settings.REGISTRY_URL,
