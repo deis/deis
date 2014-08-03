aaa95b1308450081b9d9d3b5a7e81958beaf5980
Gabriel Monroy <gabriel@opdemand.com>
2014-08-03 17:41:35 -0600

    fix(makefile): delete missing files on rsync
    fixes problems seen while working with schema migrations

--- includes.mk
+++ includes.mk
@@ -29,7 +29,7 @@ define ssh_all
 endef
 
 define rsync_all
-  for host in $(DEIS_HOSTS); do rsync -Pave "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --exclude=venv/ --exclude=.git/ --exclude='*.pyc' $(SELF_DIR)/* core@$$host:/home/core/share; done
+  for host in $(DEIS_HOSTS); do rsync -Pave "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --exclude=venv/ --exclude=.git/ --exclude='*.pyc' $(SELF_DIR)/* --delete core@$$host:/home/core/share; done
 endef
 
 define echo_cyan
