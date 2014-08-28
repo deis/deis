dce34ce3edc89f6eb12a334a2963625c46e5ab71
Gabriel Monroy <gabriel@opdemand.com>
2014-08-28 16:31:07 -0600

    fix(destroy): fix shadowing bug in destroy

--- client/scale.go
+++ client/scale.go
@@ -3,32 +3,32 @@ package client
 import "strconv"
 
 // Scale creates or destroys units to match the desired number
-func (c *FleetClient) Scale(component string, num int) (err error) {
+func (c *FleetClient) Scale(component string, requested int) (err error) {
 	for {
 		components, err := c.Units(component)
 		if err != nil {
 			return err
 		}
-		if len(components) == num {
+		if len(components) == requested {
 			break
 		}
-		if len(components) < num {
-			num, err = c.nextUnit(component)
+		if len(components) < requested {
+			num, err := c.nextUnit(component)
 			if err != nil {
 				return err
 			}
-			err := c.Create(component + "@" + strconv.Itoa(num))
+			err = c.Create(component + "@" + strconv.Itoa(num))
 			if err != nil {
 				return err
 			}
 			continue
 		}
-		if len(components) > num {
-			num, err = c.lastUnit(component)
+		if len(components) > requested {
+			num, err := c.lastUnit(component)
 			if err != nil {
 				return err
 			}
-			err := c.Destroy(component + "@" + strconv.Itoa(num))
+			err = c.Destroy(component + "@" + strconv.Itoa(num))
 			if err != nil {
 				return err
 			}
