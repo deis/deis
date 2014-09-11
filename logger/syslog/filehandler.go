b2b8cbc5c4d168c814b075d60171aab02ef0ebbe
Matthew Fisher <matthewf@opdemand.com>
2014-09-11 11:02:22 -0700

    Revert "fix(logger): properly parse incoming syslog messages"

--- logger/syslog/filehandler.go
+++ logger/syslog/filehandler.go
@@ -81,6 +81,11 @@ func (h *FileHandler) checkErr(err error) bool {
 	if err == nil {
 		return false
 	}
+	if h.l == nil {
+		log.Print(h.filename, ": ", err)
+	} else {
+		h.l.Print(h.filename, ": ", err)
+	}
 	return true
 }
 
