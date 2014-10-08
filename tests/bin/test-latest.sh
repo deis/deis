f6f11b0c2fa14719764e1a95e68f742aa90c9437
Matt Boersma <matt@opdemand.com>
2014-10-08 07:42:48 -0600

    fix(tests): correct typo in cli installer versioning

--- tests/bin/test-latest.sh
+++ tests/bin/test-latest.sh
@@ -24,13 +24,13 @@ log_phase "Installing clients"
 
 # install deis CLI from http://deis.io/ website
 pushd $DEIS_ROOT/deisctl
-curl -sSL http://deis.io/deis-cli/install.sh | sh -s v0.13.0-dev
+curl -sSL http://deis.io/deis-cli/install.sh | sh -s 0.13.0-dev
 popd
 
 # install deisctl from http://deis.io/ website
 # installs latest unit files to $HOME/.deis/units
 pushd $DEIS_ROOT/client
-curl -sSL http://deis.io/deisctl/install.sh | sh -s v0.13.0-dev
+curl -sSL http://deis.io/deisctl/install.sh | sh -s 0.13.0-dev
 popd
 
 # ensure we use distributed unit files
