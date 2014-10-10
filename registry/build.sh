90fb6e1045a6d93ed0d9a2accfe7a1d5f3d28ec1
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2014-10-10 15:26:27 -0300

    feat(registry): image without development libraries to reduce the size.

---
+++ registry/build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+DEBIAN_FRONTEND=noninteractive
+
+sed -i 's/main$/main universe/' /etc/apt/sources.list
+
+# install required packages (copied from dotcloud/docker-registry Dockerfile)
+apt-get install -y git-core build-essential python-dev \
+    libevent-dev python-openssl liblzma-dev
+
+# install pip
+curl -sSL https://raw.githubusercontent.com/pypa/pip/1.5.6/contrib/get-pip.py | python -
+
+# create a registry user
+useradd -s /bin/bash registry
+
+# add the docker registry source from github
+git clone https://github.com/deis/docker-registry /docker-registry && \
+    cd /docker-registry && \
+    git checkout 94e7707 && \
+    chown -R registry:registry /docker-registry
+
+# install boto configuration
+cp /docker-registry/config/boto.cfg /etc/boto.cfg
+cd /docker-registry && pip install -r requirements/main.txt
+
+# Install core
+pip install /docker-registry/depends/docker-registry-core
+
+# Install registry
+pip install file:///docker-registry#egg=docker-registry[bugsnag,newrelic,cors]
+
+# cleanup. indicate that python is a required package.
+apt-mark unmarkauto python python-openssl && \
+  apt-get remove -y --purge git-core build-essential python-dev && \
+  apt-get autoremove -y --purge && \
+  apt-get clean -y && \
+  rm -Rf /usr/share/man /usr/share/doc && \
+  rm -rf /tmp/* /var/tmp/* && \
+  rm -rf /var/lib/apt/lists/*
