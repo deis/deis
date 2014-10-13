b65e4995129c7c62a6224c81a68e2823f1bac370
Matthew Fisher <matthewf@opdemand.com>
2014-10-13 09:16:51 -0700

    fix(controller): inject config on top of existing environment

--- controller/registry/private.py
+++ controller/registry/private.py
@@ -59,7 +59,7 @@ def publish_release(source, config, target):
     image['id'] = _new_id()
     config['DEIS_APP'] = target_image
     config['DEIS_RELEASE'] = target_tag
-    image['config']['Env'] = _construct_env(config)
+    image['config']['Env'] = _construct_env(image['config']['Env'], config)
     # update and tag the new image
     _commit(target_image, image, _empty_tar_archive(), target_tag)
 
@@ -162,10 +162,17 @@ def _put_tag(image_id, repository_path, tag):
 
 # utility functions
 
-def _construct_env(config):
+def _construct_env(env, config):
     "Update current environment with latest config"
     new_env = []
-    # add config ENV items
+    # see if we need to update existing ENV vars
+    for e in env:
+        k, v = e.split('=', 1)
+        if k in config:
+            # update values defined by config
+            v = config.pop(k)
+        new_env.append("{}={}".format(encode(k), encode(v)))
+    # add other config ENV items
     for k, v in config.items():
         new_env.append("{}={}".format(encode(k), encode(v)))
     return new_env
