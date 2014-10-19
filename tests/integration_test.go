65a78c33fe34f556ee68f06d0f1e1b04827075e1
Matthew Fisher <matthewf@opdemand.com>
2014-10-18 17:17:49 -0700

    test(integration): add client.json tests

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -3,10 +3,16 @@
 package tests
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"os/user"
+	"path"
 	"testing"
 
 	"github.com/deis/deis/tests/utils"
 )
+
+const clientJsonFilePath string = ".deis/client.json"
 
 var (
 	gitCloneCmd  = "if [ ! -d {{.ExampleApp}} ] ; then git clone https://github.com/deis/{{.ExampleApp}}.git ; fi"
@@ -14,8 +20,38 @@ var (
 	gitPushCmd   = "git push deis master"
 )
 
+// Client represents the client data structure in ~/.deis/client.json
+type Client struct {
+	Controller string `json:"controller"`
+	Username   string `json:"username"`
+	Token      string `json:"token"`
+}
+
 func TestGlobal(t *testing.T) {
 	params := utils.GetGlobalConfig()
 	utils.Execute(t, authRegisterCmd, params, false, "")
+	clientTest(t, params)
 	utils.Execute(t, keysAddCmd, params, false, "")
+}
+
+func clientTest(t *testing.T, params *utils.DeisTestConfig) {
+	user, err := user.Current()
+	if err != nil {
+		t.Fatal(err)
+	}
+	data, err := ioutil.ReadFile(path.Join(user.HomeDir, clientJsonFilePath))
+	if err != nil {
+		t.Fatal(err)
+	}
+	client := &Client{}
+	json.Unmarshal(data, &client)
+	if client.Token == "" {
+		t.Error("token not present in client.json")
+	}
+	if client.Controller == "" {
+		t.Error("controller endpoint not present in client.json")
+	}
+	if client.Username == "" {
+		t.Error("username not present in client.json")
+	}
 }
