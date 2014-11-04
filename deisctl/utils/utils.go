3c49fda28f1f12e674642bd4b3e4b3009311cd14
Matt Boersma <matt@opdemand.com>
2014-11-04 13:45:09 -0700

    ref(deisctl): remove unused utility code
    Also capitalize NewUUID() as recommended by `golint`.

--- deisctl/utils/utils.go
+++ deisctl/utils/utils.go
@@ -3,38 +3,31 @@
 package utils
 
 import (
-	_ "bufio"
 	"bytes"
-	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"net"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
-	"syscall"
 	"time"
 
 	"github.com/coreos/go-etcd/etcd"
 	"github.com/deis/deis/deisctl/constant"
-	_ "github.com/docker/docker/api/client"
 	uuid "github.com/satori/go.uuid"
 )
 
-// NewUuid returns a new V4-style unique identifier.
-func NewUuid() string {
+// NewUUID returns a new V4-style unique identifier.
+func NewUUID() string {
 	u1 := uuid.NewV4()
 	s1 := fmt.Sprintf("%s", u1)
 	return strings.Split(s1, "-")[0]
 }
 
 func getetcdClient() *etcd.Client {
-
 	machines := []string{"http://127.0.0.1:4001/"}
-	c := etcd.NewClient(machines)
-	return c
+	return etcd.NewClient(machines)
 }
 
 // GetKey returns the value of an etcd key, or of an environment variable if etcd didn't have
@@ -48,20 +41,11 @@ func GetKey(dir, key, perm string) string {
 	return result.Node.Value
 }
 
-func PullImage(args string) error {
-	cmdl := exec.Command("docker", "pull", args)
-	if _, _, err := RunCommandWithStdoutStderr(cmdl); err != nil {
-		fmt.Println("(Error )")
-		return err
-	}
-	return nil
-}
-
 // GetClientID returns the CoreOS Machine ID, or an unknown UUID string.
 func GetClientID() string {
 	machineID := GetMachineID("/")
 	if machineID == "" {
-		return fmt.Sprintf("{unknown-" + NewUuid() + "}")
+		return fmt.Sprintf("{unknown-" + NewUUID() + "}")
 	}
 	return machineID
 }
@@ -85,31 +69,6 @@ func GetVersion() string {
 	return strings.TrimSpace(string(id))
 }
 
-// GetFileBytes returns a byte array of the contents of a file.
-func GetFileBytes(filename string) []byte {
-	file, _ := os.Open(filename)
-	defer file.Close()
-	stat, _ := file.Stat()
-	bs := make([]byte, stat.Size())
-	_, _ = file.Read(bs)
-	return bs
-}
-
-func ListFiles(dir string) ([]string, error) {
-	files, err := filepath.Glob(dir)
-	return files, err
-}
-
-// CreateFile creates an empty file at the specified path.
-func CreateFile(path string) error {
-	fo, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer fo.Close()
-	return nil
-}
-
 // Extract expands a .tar archive file into the specified directory.
 func Extract(file, dir string) (err error) {
 	var wd, _ = os.Getwd()
@@ -123,72 +82,9 @@ func Extract(file, dir string) (err error) {
 	return nil
 }
 
-// GetUserDetails returns sections of a UUID.
-func GetUserDetails() (string, string) {
-	u1 := uuid.NewV4()
-	s1 := fmt.Sprintf("%s", u1)
-	return strings.Split(s1, "-")[0], strings.Split(s1, "-")[1]
-}
-
-// GetHostOs returns either "darwin" or "ubuntu".
-func GetHostOs() string {
-	cmd := exec.Command("uname")
-	out, _ := cmd.Output()
-	if strings.Contains(string(out), "Darwin") {
-		return "darwin"
-	}
-	return "ubuntu"
-}
-
-// GetHostIPAddress returns the host IP for accessing etcd and Deis services.
-func GetHostIPAddress() string {
-	IP := os.Getenv("HOST_IPADDR")
-	if IP == "" {
-		IP = "172.17.8.100"
-	}
-	return IP
-}
-
 // PutVersion updates the package version to a local text file resource.
 func PutVersion(version string) error {
-	err := ioutil.WriteFile(constant.Version, []byte(version), 0644)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// Append grows a string array by appending a new element.
-func Append(slice []string, data string) []string {
-	m := len(slice)
-	n := m + 1
-	if n > cap(slice) { // if necessary, reallocate
-		// allocate double what's needed, for future growth.
-		newSlice := make([]string, (n + 1))
-		copy(newSlice, slice)
-		slice = newSlice
-	}
-	slice = slice[0:n]
-	slice[n-1] = data
-	return slice
-}
-
-// GetRandomPort returns an unused TCP listen port on the host.
-func GetRandomPort() string {
-	l, _ := net.Listen("tcp", "127.0.0.1:0") // listen on localhost
-	defer l.Close()
-	port := l.Addr()
-	return strings.Split(port.String(), ":")[1]
-}
-
-func getExitCode(err error) (int, error) {
-	exitCode := 0
-	if exiterr, ok := err.(*exec.ExitError); ok {
-		if procExit := exiterr.Sys().(syscall.WaitStatus); ok {
-			return procExit.ExitStatus(), nil
-		}
-	}
-	return exitCode, fmt.Errorf("failed to get exit code")
+	return ioutil.WriteFile(constant.Version, []byte(version), 0644)
 }
 
 // RunCommandWithStdoutStderr execs a command and returns its output.
@@ -231,37 +127,6 @@ func Execute(script string) error {
 		return err
 	}
 	return nil
-}
-
-func Timeout(msg string, seconds time.Duration, f func()) error {
-	c := make(chan bool)
-	// Make sure we are not too long
-	go func() {
-		time.Sleep(seconds)
-		c <- true
-	}()
-	go func() {
-		f()
-		c <- false
-	}()
-	if <-c && msg != "" {
-		return errors.New(msg + "timed out")
-	}
-	return nil
-}
-
-func logDone(message string) {
-	fmt.Printf("[PASSED]: %s\n", message)
-}
-
-func stripTrailingCharacters(target string) string {
-	target = strings.Trim(target, "\n")
-	target = strings.Trim(target, " ")
-	return target
-}
-
-func nLines(s string) int {
-	return strings.Count(s, "\n")
 }
 
 // DeisIfy returns a pretty-printed deis logo along with the corresponding message
