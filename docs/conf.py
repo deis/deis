7607a5c6ff1d5c414219a1595db2e65339bf8f53
Matt Boersma <matt@opdemand.com>
2014-11-14 14:47:45 -0700

    ref(docs): silence "not in TOC" warnings for files starting with "_"
    Let's start reStructuredText include fragments with underscores as a
    convention to help us and Sphinx recognize they aren't standalone
    documents.

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,7 @@ release = __version__
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'venv']
+exclude_patterns = ['_build', 'venv', '**/_*.rst']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
