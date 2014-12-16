97d6810c5551301d5054e5bcd20cc56f25600b61
Chris Armstrong <chris@opdemand.com>
2014-12-16 15:19:47 -0800

    tests(utils): add unit tests for utils package

---
+++ deisctl/utils/utils_test.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"os"
+	"testing"
+)
+
+// TestResolvePath ensures ResolvePath replaces $HOME and ~ as expected
+func TestResolvePath(t *testing.T) {
+	paths := []string{"$HOME/foo/bar", "~/foo/bar"}
+	expected := os.Getenv("HOME") + "/foo/bar"
+	for _, path := range paths {
+		resolved := ResolvePath(path)
+		if resolved != expected {
+			t.Errorf("Test failed: expected %s, got %s", expected, resolved)
+		}
+	}
+}
