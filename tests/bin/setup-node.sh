883c4f91b1897bc5acd7d4fca84752915d447fea
Matt Boersma <matt@opdemand.com>
2015-02-28 08:26:50 -0700

    fix(builder): install docker from get.docker.com

--- tests/bin/setup-node.sh
+++ tests/bin/setup-node.sh
@@ -5,7 +5,7 @@
 
 # install docker
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 apt-get update && apt-get install -yq lxc-docker-1.5.0
 
 # install java
