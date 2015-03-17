b07a0534b9a0e1c24593fb26e3a49c22578ab28a
Matt Boersma <matt@opdemand.com>
2015-03-17 13:27:23 -0600

    feat(controller): print stack trace when worker is killed

--- controller/deis/gconf.py
+++ controller/deis/gconf.py
@@ -7,3 +7,17 @@ loglevel = 'info'
 errorlog = '-'
 accesslog = '-'
 access_log_format = '%(h)s "%(r)s" %(s)s %(b)s "%(a)s"'
+
+
+def worker_int(worker):
+    """Print a stack trace when a worker receives a SIGINT or SIGQUIT signal."""
+    worker.log.warning('worker terminated')
+    import traceback
+    traceback.print_stack()
+
+
+def worker_abort(worker):
+    """Print a stack trace when a worker receives a SIGABRT signal, generally on timeout."""
+    worker.log.warning('worker aborted')
+    import traceback
+    traceback.print_stack()
