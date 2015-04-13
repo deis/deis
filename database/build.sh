914d88bf022bb2f2763e04faf45c8767b5b76aae
Matt Boersma <mboersma@engineyard.com>
2015-04-13 14:23:21 -0400

    chore(*): update pip installer tool to 6.1.1

--- database/build.sh
+++ database/build.sh
@@ -32,7 +32,7 @@ apt-get update && apt-get install -yq \
 /etc/init.d/postgresql stop
 
 # install pip
-curl -sSL https://raw.githubusercontent.com/pypa/pip/6.0.8/contrib/get-pip.py | python -
+curl -sSL https://raw.githubusercontent.com/pypa/pip/6.1.1/contrib/get-pip.py | python -
 
 # install wal-e
 cd /tmp
