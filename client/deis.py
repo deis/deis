63c0c932b7cd8a76d61a29ff2ee8587391166d56
Matt Boersma <mboersma@engineyard.com>
2015-04-22 11:54:08 -0600

    chore(release): update version to v1.5.2

--- client/deis.py
+++ client/deis.py
@@ -72,7 +72,7 @@ import requests
 from tabulate import tabulate
 from termcolor import colored
 
-__version__ = '1.5.1'
+__version__ = '1.5.2'
 
 # what version of the API is this client compatible with?
 __api_version__ = '1.2'
