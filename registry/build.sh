20c51c5a3112894989a4b22b0345a88653171cec
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2015-05-11 02:02:36 -0300

    fix(registry): python workaround for ssl connections if > 2.7.8

--- registry/build.sh
+++ registry/build.sh
@@ -14,21 +14,25 @@ fi
 apk add --update-cache \
   build-base \
   git \
-  libevent-dev \
   openssl-dev \
   python-dev \
+  libffi-dev \
   swig \
+  libevent-dev \
   xz-dev
 
 # install pip
 curl -sSL https://raw.githubusercontent.com/pypa/pip/6.1.1/contrib/get-pip.py | python -
+
+# workaround to python > 2.7.8 SSL issues
+pip install pyopenssl ndg-httpsclient pyasn1
 
 # create a registry user
 adduser -D -s /bin/bash registry
 
 # add the docker registry source from github
 git clone -b new-repository-import-v091 --single-branch https://github.com/deis/docker-registry /docker-registry && \
-    chown -R registry:registry /docker-registry
+  chown -R registry:registry /docker-registry
 
 # install boto configuration
 cp /docker-registry/config/boto.cfg /etc/boto.cfg
@@ -49,4 +53,5 @@ apk del --purge \
   build-base \
   linux-headers \
   python-dev
+
 rm -rf /var/cache/apk/*
