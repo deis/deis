ef74a43fce3418eb8c92c7a3f230b1d19ce70be0
Joshua Anderson <janderson@engineyard.com>
2015-07-20 15:15:30 -0700

    feat(client-go): add tags endpoint

---
+++ client-go/cmd/tags.go
@@ -0,0 +1,128 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/deis/deis/pkg/prettyprint"
+
+	"github.com/deis/deis/client-go/controller/api"
+	"github.com/deis/deis/client-go/controller/models/config"
+)
+
+// TagsList lists an app's tags.
+func TagsList(appID string) error {
+	c, appID, err := load(appID)
+
+	if err != nil {
+		return err
+	}
+
+	config, err := config.List(c, appID)
+
+	fmt.Printf("=== %s Tags\n", appID)
+
+	tagMap := make(map[string]string)
+
+	for key, value := range config.Tags {
+		tagMap[key] = value.(string)
+	}
+
+	fmt.Print(prettyprint.PrettyTabs(tagMap, 5))
+
+	return nil
+}
+
+// TagsSet sets an app's tags.
+func TagsSet(appID string, tags []string) error {
+	c, appID, err := load(appID)
+
+	if err != nil {
+		return err
+	}
+
+	tagsMap := parseTags(tags)
+
+	fmt.Print("Applying tags... ")
+
+	quit := progress()
+	configObj := api.Config{}
+	configObj.Tags = tagsMap
+
+	_, err = config.Set(c, appID, configObj)
+
+	quit <- true
+	<-quit
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Print("done\n\n")
+
+	return TagsList(appID)
+}
+
+// TagsUnset removes an app's tags.
+func TagsUnset(appID string, tags []string) error {
+	c, appID, err := load(appID)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Print("Applying tags... ")
+
+	quit := progress()
+
+	configObj := api.Config{}
+
+	tagsMap := make(map[string]interface{})
+
+	for _, tag := range tags {
+		tagsMap[tag] = nil
+	}
+
+	configObj.Tags = tagsMap
+
+	_, err = config.Set(c, appID, configObj)
+
+	quit <- true
+	<-quit
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Print("done\n\n")
+
+	return TagsList(appID)
+}
+
+func parseTags(tags []string) map[string]interface{} {
+	tagMap := make(map[string]interface{})
+
+	for _, tag := range tags {
+		key, value, err := parseTag(tag)
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		tagMap[key] = value
+	}
+
+	return tagMap
+}
+
+func parseTag(tag string) (string, string, error) {
+	parts := strings.Split(tag, "=")
+
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf(`%s is invalid, Must be in format key=value
+Examples: rack=1 evironment=production`, tag)
+	}
+
+	return parts[0], parts[1], nil
+}
---
+++ client-go/parser/tags.go
@@ -0,0 +1,116 @@
+package parser
+
+import (
+	"fmt"
+
+	"github.com/deis/deis/client-go/cmd"
+	docopt "github.com/docopt/docopt-go"
+)
+
+// Tags routes tags commands to their specific function
+func Tags(argv []string) error {
+	usage := `
+Valid commands for tags:
+
+tags:list        list tags for an app
+tags:set         set tags for an app
+tags:unset       unset tags for an app
+
+Use 'deis help [command]' to learn more.
+`
+	if len(argv) < 2 {
+		return tagsList([]string{"tags:list"})
+	}
+
+	switch argv[1] {
+	case "list":
+		return tagsList(combineCommand(argv))
+	case "set":
+		return tagsSet(combineCommand(argv))
+	case "unset":
+		return tagsUnset(combineCommand(argv))
+	case "--help":
+		fmt.Print(usage)
+		return nil
+	default:
+		PrintUsage()
+		return nil
+	}
+}
+
+func tagsList(argv []string) error {
+	usage := `
+Lists tags for an application.
+
+Usage: deis tags:list [options]
+
+Options:
+  -a --app=<app>
+    the uniquely identifiable name of the application.
+`
+
+	args, err := docopt.Parse(usage, argv, true, "", false, true)
+
+	if err != nil {
+		return err
+	}
+
+	return cmd.TagsList(safeGetValue(args, "--app"))
+}
+
+func tagsSet(argv []string) error {
+	usage := `
+Sets tags for an application.
+
+A tag is a key/value pair used to tag an application's containers and is passed to the
+scheduler. This is often used to restrict workloads to specific hosts matching the
+scheduler-configured metadata.
+
+Usage: deis tags:set [options] <key>=<value>...
+
+Arguments:
+  <key> the tag key, for example: "environ" or "rack"
+  <value> the tag value, for example: "prod" or "1"
+
+Options:
+  -a --app=<app>
+    the uniquely identifiable name for the application.
+`
+
+	args, err := docopt.Parse(usage, argv, true, "", false, true)
+
+	if err != nil {
+		return err
+	}
+
+	app := safeGetValue(args, "--app")
+	tags := args["<key>=<value>"].([]string)
+
+	return cmd.TagsSet(app, tags)
+}
+
+func tagsUnset(argv []string) error {
+	usage := `
+Unsets tags for an application.
+
+Usage: deis tags:unset [options] <key>...
+
+Arguments:
+  <key> the tag key to unset, for example: "environ" or "rack"
+
+Options:
+  -a --app=<app>
+    the uniquely identifiable name for the application.
+`
+
+	args, err := docopt.Parse(usage, argv, true, "", false, true)
+
+	if err != nil {
+		return err
+	}
+
+	app := safeGetValue(args, "--app")
+	tags := args["<key>"].([]string)
+
+	return cmd.TagsUnset(app, tags)
+}
