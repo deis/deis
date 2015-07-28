8420b83780c79fde67157c9d400c7bd5294dfc77
Joshua Anderson <janderson@engineyard.com>
2015-07-28 14:56:32 -0700

    test(router): run shell scripts through shellcheck

--- router/build.sh
+++ router/build.sh
@@ -11,14 +11,14 @@ if [[ -z $DOCKER_BUILD ]]; then
 fi
 
 function get_src {
-  hash=$1
-  url=$2
-  f=$(basename $url)
+  hash="$1"
+  url="$2"
+  f=$(basename "$url")
 
-  curl -sSL $url -o $f
+  curl -sSL "$url" -o "$f"
   echo "$hash  $f" | sha256sum -c - || exit 10
-  tar xzf $f
-  rm $f
+  tar xzf "$f"
+  rm "$f"
 }
 
 export VERSION_NGINX=nginx-1.9.2
@@ -31,11 +31,11 @@ export BUILD_PATH=/tmp/build
 # nginx installation directory
 export PREFIX=/opt/nginx
 
-rm -rf $PREFIX
-mkdir $PREFIX
+rm -rf "$PREFIX"
+mkdir "$PREFIX"
 
-mkdir $BUILD_PATH
-cd $BUILD_PATH
+mkdir "$BUILD_PATH"
+cd "$BUILD_PATH"
 
 # install required packages to build
 apk add --update-cache \
@@ -52,22 +52,22 @@ apk add --update-cache \
 
 # download, verify and extract the source files
 get_src 80b6425be14a005c8cb15115f3c775f4bc06bf798aa1affaee84ed9cf641ed78 \
-        http://nginx.org/download/$VERSION_NGINX.tar.gz
+        "http://nginx.org/download/$VERSION_NGINX.tar.gz"
 
 get_src 128b56873eedbd3f240dc0f88a8b260d791321db92f14ba2fc5c49fc5307e04d \
-        https://github.com/nbs-system/naxsi/archive/$VERSION_NAXSI.tar.gz
+        "https://github.com/nbs-system/naxsi/archive/$VERSION_NAXSI.tar.gz"
 
 get_src 501f299abdb81b992a980bda182e5de5a4b2b3e275fbf72ee34dd7ae84c4b679 \
-        https://github.com/simpl/ngx_devel_kit/archive/v$VERSION_NDK.tar.gz
+        "https://github.com/simpl/ngx_devel_kit/archive/v$VERSION_NDK.tar.gz"
 
 get_src 8d280fc083420afb41dbe10df9a8ceec98f1d391bd2caa42ebae67d5bc9295d8 \
-        https://github.com/openresty/set-misc-nginx-module/archive/v$VERSION_SETMISC.tar.gz
+        "https://github.com/openresty/set-misc-nginx-module/archive/v$VERSION_SETMISC.tar.gz"
 
 # build nginx
-cd $BUILD_PATH/$VERSION_NGINX
+cd "$BUILD_PATH/$VERSION_NGINX"
 
 ./configure \
-  --prefix=$PREFIX \
+  --prefix="$PREFIX" \
   --pid-path=/run/nginx.pid \
   --with-debug \
   --with-pcre-jit \
@@ -85,9 +85,9 @@ cd $BUILD_PATH/$VERSION_NGINX
   --with-mail \
   --with-mail_ssl_module \
   --with-stream \
-  --add-module=$BUILD_PATH/naxsi-$VERSION_NAXSI/naxsi_src \
-  --add-module=$BUILD_PATH/ngx_devel_kit-$VERSION_NDK \
-  --add-module=$BUILD_PATH/set-misc-nginx-module-$VERSION_SETMISC \
+  --add-module="$BUILD_PATH/naxsi-$VERSION_NAXSI/naxsi_src" \
+  --add-module="$BUILD_PATH/ngx_devel_kit-$VERSION_NDK" \
+  --add-module="$BUILD_PATH/set-misc-nginx-module-$VERSION_SETMISC" \
   && make && make install
 
 mv /tmp/firewall /opt/nginx/firewall
