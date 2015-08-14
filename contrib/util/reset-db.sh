acecb930ba53de781c49537815eb0b7fd9267a93
Joshua Anderson <janderson@engineyard.com>
2015-08-14 15:16:36 -0700

    fix(contib): remove nonfunctional scripts of dubious usefulness

--- contrib/util/reset-db.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash -e
-
-VP_HOST="$(hostname | sed -e 's/^ *//g' -e 's/ *$//g').local"
-VP_USER=$(whoami | sed -e 's/^ *//g' -e 's/ *$//g')
-
-vagrant ssh -c "sudo VP_HOST=$VP_HOST VP_USER=$VP_USER /vagrant/contrib/vagrant/util/_controller-reset-db.sh"
-
-echo "Logging in user 'devuser' with password 'devpass'..."
-deis login "deis-controller.local:8000" --username=devuser --password=devpass
