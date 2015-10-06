d5b7ffa7b4f20ee6a12e02940008b9a2514b6a31
Matt Boersma <mboersma@deis.com>
2015-10-06 15:12:34 -0600

    chore(release): update version in master to v1.12.0-dev

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version identifies this Deis product revision.
-const Version = "1.11.0"
+const Version = "1.12.0-dev"
 
 // API identifies the latest Deis api verison
 const APIVersion = "1.7"
