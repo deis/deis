c85531ab990a05a9ff9b3eec7f48efd2901b031f
Matt Boersma <mboersma@deis.com>
2016-05-06 10:28:10 -0600

    chore(release): update version to v1.13.1

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version identifies this Deis product revision.
-const Version = "1.13.0"
+const Version = "1.13.1"
 
 // API identifies the latest Deis api verison
 const APIVersion = "1.7"
